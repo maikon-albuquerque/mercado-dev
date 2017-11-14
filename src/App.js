b59e501c548bb2faa1b4f31b7060385a4a112870
FRANCISCO MAIKON ALBUQUERQUE DE MORAIS <maikonmorais@edu.unifor.br>
2017-11-14 02:42:49 -0300

    Integração com firebase BIND

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import base from './base'
 
 import HeaderHome from './HeaderHome'
 import AnuncioHome from './AnuncioHome'
@@ -6,22 +7,46 @@ import LinkCategoria from './LinkCategoria'
 import Footer from './Footer'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      categorias: [],
+      anuncios: []
+    }
+    base.bindToState('categorias',{
+      context: this,
+      state: 'categorias'
+    })
+    base.bindToState('anuncios',{
+      context: this,
+      state: 'anuncios',
+      queries:{
+        limitToLast: 3
+      }
+    })
+  }
   render() {
+    let index = 0
     return (
       <div className="App">
         <HeaderHome />
         <div className="container">
         <h3>Ultimos Anúncios</h3>
-        <div className="row">
-        <AnuncioHome />
-        <AnuncioHome />
-        <AnuncioHome />
-        </div>
+          <div className="row">
+            { this.state.anuncios.map(( anuncio, indice ) => {
+              return <AnuncioHome key={indice} anuncio={anuncio} />
+            })}
+          </div>
         <h3>Categorias</h3>
-        <LinkCategoria />
-        <LinkCategoria />
-        <LinkCategoria />
-        <LinkCategoria />
+          <div className="row">
+          { this.state.categorias.map( (cat, indice) => {
+            return [
+                    <LinkCategoria categoria={cat} key={indice} />,
+                    ++index%4 === 0 && <div key={'c'+indice} className="w-100"></div>
+                  ]
+          })}
+          </div>
         </div>
         <Footer />
       </div>
